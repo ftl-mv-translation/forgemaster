d940b577b02e254fbfa346676d1fe92c4932c7a2
ranhai613 <hulanhai613@gmail.com>
2024-11-15 10:31:24 +0900

    Abandon writelist output

--- snippets/ci-nightly.sh
+++ snippets/ci-nightly.sh
@@ -7,7 +7,6 @@ mvloc batch-apply $1
 mvloc package $1
 
 mv report.txt "packages/batch-apply-report-$1.txt"
-mv writelist.json "packages/package-report-$1.json"
 if [ -f ./metadata.xml/$1.xml ]; then
     cp "metadata.xml/$1.xml" "packages/metadata-$1.xml"
 else
